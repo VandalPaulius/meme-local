ca42bbf69d0f112684551479b710df334f9341a5
Paulius Juska <paulius@monito.com>
2023-03-26 19:55:11 +0300

    added husky and lint-staged

--- src/app.jsx
+++ src/app.jsx
@@ -1,10 +1,5 @@
 import * as React from "react";
 import * as ReactDOMClient from "react-dom/client";
 
-const test = {
-  la: "la",
-        la: "la2",
-};
-
 const root = ReactDOMClient.createRoot(document.body);
 root.render(<h2>Hello from React!</h2>);
