087a0dc433e980f0adf50146b1c685d8d9636266
Paulius Juska <paulius@monito.com>
2023-03-26 19:48:44 +0300

    test

--- src/app.jsx
+++ src/app.jsx
@@ -1,5 +1,10 @@
 import * as React from "react";
 import * as ReactDOMClient from "react-dom/client";
 
+const test = {
+  la: "la",
+        la: "la2",
+};
+
 const root = ReactDOMClient.createRoot(document.body);
- root.render(<h2>Hello from React!</h2>);
+root.render(<h2>Hello from React!</h2>);
